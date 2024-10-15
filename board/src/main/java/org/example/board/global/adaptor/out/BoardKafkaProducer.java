608629d2ce479de67ac302ae86e255449ad4799a
NakyungSong <thdsk4311@sju.ac.krgit config --global user.email thdsk4311@sju.ac.krgit config --list>
2024-10-15 17:14:01 +0900

    게시글 등록 Kafka 이벤트 발행 및 소비 로직 추가

--- board/src/main/java/org/example/board/global/adaptor/out/BoardKafkaProducer.java
+++ board/src/main/java/org/example/board/global/adaptor/out/BoardKafkaProducer.java
@@ -1,10 +1,17 @@
 package org.example.board.global.adaptor.out;
 
 import lombok.RequiredArgsConstructor;
+import org.example.board.domain.board.model.event.BoardRegisterEvent;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class BoardKafkaProducer {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void sendBoardRegisterEvent(BoardRegisterEvent event) {
+        kafkaTemplate.send("board-register", event);
+    }
 }
 
