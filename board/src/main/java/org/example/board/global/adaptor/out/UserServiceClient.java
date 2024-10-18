58d8594a2d114f433e05b457e836fa7884a81a53
NakyungSong <thdsk4311@sju.ac.krgit config --global user.email thdsk4311@sju.ac.krgit config --list>
2024-10-18 09:37:41 +0900

    오픈페인

--- board/src/main/java/org/example/board/global/adaptor/out/UserServiceClient.java
+++ board/src/main/java/org/example/board/global/adaptor/out/UserServiceClient.java
@@ -3,9 +3,9 @@ package org.example.board.global.adaptor.out;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "company-service", url = "http://localhost:8080")
+@FeignClient(name = "user-service", url = "http://localhost:8000/api")
 public interface UserServiceClient {
 
-    @GetMapping("/company/companyIdx")
+    @GetMapping("/company/company-idx")
     Long getCompanyIdx();  // companyIdx를 가져오는 API 호출
 }
